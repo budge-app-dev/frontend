bcecfa2216263a655dec3b5791738f5e2ac0706f
Michelle Chen <mchen0062@gmail.com>
2021-03-16 00:26:19 -0700

    added actions and reducers for categories. Brought in categories on page

--- budge/src/Components/reducers/index.js
+++ budge/src/Components/reducers/index.js
@@ -1,15 +1,30 @@
-import { ADD_CAT } from '../actions'
+import { GET_CAT_SUCCESS, GET_CAT_FAIL, ADD_CAT } from '../actions'
 
 export const initialState = { 
     categories: [], 
-    addCat: [],
+    userCat: [],
     totalBudget: 0, 
     income: 0,
-    isLoading: false 
+    error:''
 }
 
 export const reducer = (state= initialState, action) => {
     switch(action.type){
+        case(GET_CAT_SUCCESS):
+        return({
+            ...state, 
+            categories: action.payload
+        })
+        case(GET_CAT_FAIL):
+        return({
+            ...state, 
+            error: action.payload
+        })
+        case(ADD_CAT):
+        return({
+            ...state, 
+            userCat: [...state.userCat, action.payload]
+        })
         default:
             return state
     }
