79d46c1b5fb0e03aedc78bc8787a4c403220ef64
juancaruizc <juancruizcruz@gmail.com>
2021-03-16 21:16:56 -0700

    created left side view

--- budge/src/Components/dashboard/NavBar.js
+++ budge/src/Components/dashboard/NavBar.js
@@ -4,10 +4,12 @@ import testImage from '../../images/testImage.jpg';
 import BudgeLogo from '../../images/budgeappBlue.svg';
 
 const Container = styled.div`
-  div {
+  div.navBar {
     display: flex;
     height: 10vh;
     box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+    /* z-index: 2000;
+    position: absolute; */
   }
 
   img.profile {
@@ -20,16 +22,32 @@ const Container = styled.div`
   }
 
   img.logo {
-    margin-left: 1%;
+    margin-left: 2%;
+  }
+
+  div.leftSideView {
+    background-color: #fafbfc;
+    width: 16%;
+    height: 90vh;
+  }
+
+  h2 {
+    color: #2a7de1;
+    font-size: 1.1rem;
+    margin-top: 0.5%;
+    padding-top: 12%;
   }
 `;
 
 function NavBar() {
   return (
     <Container>
-      <div>
+      <div class='navBar'>
         <img class='logo' src={BudgeLogo} alt=''></img>
         <img class='profile' src={testImage} alt=''></img>
+      </div>
+      <div class='leftSideView'>
+        <h2>Welcome Bob</h2>
       </div>
     </Container>
   );
