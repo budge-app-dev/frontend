af062e70889c5c011b13972b22015c7a2d6ef947
juancaruizc <juancruizcruz@gmail.com>
2021-03-23 16:22:38 -0700

    updated

--- budge/src/Components/dashboard/budgeSection.jsx
+++ budge/src/Components/dashboard/budgeSection.jsx
@@ -5,7 +5,7 @@ import { Bar } from 'react-chartjs-2';
 
 let newDate = new Date();
 let date = newDate.getDate();
-let month = newDate.getMonth() + 1;
+let month = newDate.getMonth();
 let year = newDate.getFullYear();
 let day = newDate.getDay();
 
